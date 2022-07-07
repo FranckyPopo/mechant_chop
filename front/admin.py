5ca3678f50f8aa97f322820cd881e0977d37e03a
FranckyPoop <afrifranck2003@gmail.com>
2022-07-07 12:18:53 +0000

    Ajout des models dans l'espace admin

--- front/admin.py
+++ front/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from front.models import SiteInfo
+
+@admin.register(models.SiteInfo)
+class SiteInfo(admin.ModelAdmin):
+    list_display = ["email", "phone", "date_create", "date_update"]
--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop import models
+
+@admin.register(models.Brand)
+class Brand(admin.ModelAdmin):
+    list_display = ["name", "active", "date_create", "date_update"]
+    
+@admin.register(models.Category)
+class Category(admin.ModelAdmin):
+    list_display = ["name", "active", "date_create", "date_update"]
+    
+@admin.register(models.GlobalSold)
+class GlobalSold(admin.ModelAdmin):
+    list_display = ["name", "description", "date_create", "date_update"]
+    
+@admin.register(models.Size)
+class Size(admin.ModelAdmin):
+    list_display = ["name", "active", "date_create", "date_update"]
+    
+@admin.register(models.Product)
+class Product(admin.ModelAdmin):
+    list_display = ["name", "brand", "date_create", "date_update"]
+    
+@admin.register(models.ImageProduct)
+class ImageProduct(admin.ModelAdmin):
+    list_display = ["image", "date_create", "date_update"]
+    
+@admin.register(models.NewsLater)
+class NewsLater(admin.ModelAdmin):
+    list_display = ["email", "date_create", "date_update"]
