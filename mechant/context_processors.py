7fdb655a96fc4cebe09a6f12b763468fec59db19
FranckyPoop <afrifranck2003@gmail.com>
2022-07-07 19:28:46 +0000

    Création de fonction list_product dans le context_preccessors

--- mechant/context_processors.py
+++ mechant/context_processors.py
@@ -3,3 +3,7 @@ from shop import models
 
 def list_category(request):
     return {"categories": models.Category.objects.all().filter(active=True)}
+
+
+def list_product(request):
+    return {"products": models.Product.objects.all().filter(active=True)}
--- mechant/settings.py
+++ mechant/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'mechant.context_processors.list_category',
+                'mechant.context_processors.list_product',
             ],
         },
     },
@@ -131,3 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentication.User" 
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
