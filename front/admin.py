52b4310a6149f1670c81f87ef9b0aa9ce41e0312
FranckyPoop <afrifranck2003@gmail.com>
2022-07-07 12:21:29 +0000

    Amélioration du fichier admin de l'application front

--- front/admin.py
+++ front/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from front.models import SiteInfo
 
-@admin.register(models.SiteInfo)
+@admin.register(SiteInfo)
 class SiteInfo(admin.ModelAdmin):
     list_display = ["email", "phone", "date_create", "date_update"]
