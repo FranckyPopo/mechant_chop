d6a898082bab5a5aa077da33c6b5c20ff3fde10b
FranckyPoop <afrifranck2003@gmail.com>
2022-08-03 15:24:50 +0000

    Création de la méthode add_to_cart_session

--- shop/views.py
+++ shop/views.py
@@ -12,33 +12,56 @@ class ProductAddCart(View):
     template_name = "shop/pages/cart-list.html"
     
     def post(self, request, product_pk):
-        session_id = request.session._get_or_create_session_key()
-        product = models.Product.objects.get(pk=product_pk)
-        
-        objet, created = models.OrderItem.objects.get_or_create(
-            session_id=session_id,
-            product=product,
-        )     
-        
-        if not created:
-            objet.quantity += 1
-            objet.save()
-                
-        cart = models.OrderItem.objects.filter(session_id=session_id)
-        response = render_to_string(self.template_name, context={"cart": cart})
+        if request.user.is_authenticated:
+            pass
+        else:
+            self.add_to_cart_session(request, int(product_pk))
         
         return HttpResponse(
-            response,
+            "",
             headers={
                 "HX-Trigger": json.dumps({
                     "product_add": {
-                        "total_product": utils.sum_quantity_cart(session_id),
-                        "tota_price_cart": utils.sum_price_cart(session_id),
+                        "total_product": None,
+                        "tota_price_cart": None,
                     }
                 })
             }
         )
     
+    def add_to_cart_session(self, request, product_pk: int) -> None:
+        """Cette méthode va permtre d'ajouter des produits
+        dans un panier via la session de l'utilisateur"""
+        
+        if not request.session.session_key: request.session.save()
+        quantity = request.POST.get("quantity", False)
+        cart = request.session.get("cart", False)
+            
+        if quantity:
+            for order in cart:
+                if order["pk"] == product_pk:
+                    order["quantity"] = int(quantity)
+                    request.session["cart"] = cart
+                    break
+        elif cart:
+            for order in cart:
+                if order["pk"] == product_pk:
+                    order["quantity"] += 1
+                    request.session["cart"] = cart
+                    break
+            else:
+                order = {
+                    "pk": product_pk,
+                    "quantity": 1
+                }
+                cart.append(order)
+                request.session["cart"] = cart
+        elif not cart:
+            request.session["cart"] = [
+                {"pk": product_pk, "quantity": 1}
+            ]
+        print(cart)
+
     def http_method_not_allowed(self, request):
         return redirect("shop_index")
     
@@ -68,13 +91,12 @@ class ProductDeleteCart(View):
         return redirect("shop_index")
 
 def shop_index(request):
-    session_id = request.session._get_or_create_session_key()
-    cart = models.OrderItem.objects.filter(session_id=session_id)
-    data = {
-        "cart": cart,
+
+    context = {
+        "cart": None,
     }
     
-    return render(request, "shop/pages/index.html", context=data)
+    return render(request, "shop/pages/index.html", context=context)
 
 def shop_detail_product(request):
     return render(request, "shop/pages/single-product-details.html")
